a392a47d2e93778f7ce552195b72583460044e08
Akeret, Joel <joel.akeret@gmail.com>
2020-05-19 11:46:40 +0200

    grid search

---
+++ tuning.py
@@ -0,0 +1,42 @@
+import argparse
+
+import numpy as np
+from sklearn.model_selection import ParameterGrid
+
+import robotics
+
+
+def tune_model(data_path="", log_path="logs", output_path="model",
+               learning_rate=0.001, batch_size=32, epochs=10):
+    param_grid = dict(
+        depth=[3, 4, 5],
+        kernel_size=[3, 4, 5, 6],
+        filters=[16, 32, 64, 96, 128, 256],
+        dropout_rate=np.arange(0.0, 0.81, 0.2),
+        normalize_inputs=[True, False]
+    )
+
+    for hyperparams in ParameterGrid(param_grid):
+        robotics.run_training(data_path, log_path, output_path,
+                              learning_rate=learning_rate, batch_size=batch_size, epochs=epochs,
+                              **hyperparams)
+
+
+def _parse_cli_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--data-path', type=str, default="./")
+    parser.add_argument('--output-path', type=str, default="model")
+    parser.add_argument('--log-path', type=str, default="logs")
+
+    parser.add_argument('--learning_rate', type=int, default=0.001)
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--epochs', type=int, default=1)
+
+    args = vars(parser.parse_args())
+
+    return args
+
+
+if __name__ == '__main__':
+    tune_model(**_parse_cli_arguments())
