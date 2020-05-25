ec03695bbb4519d1a553474747fe84ac00b25aba
Akeret, Joel <joel.akeret@gmail.com>
2020-05-25 09:29:13 +0200

    reducing param space

--- tuning.py
+++ tuning.py
@@ -7,16 +7,19 @@ import robotics
 
 
 def tune_model(data_path="", log_path="logs", output_path="model",
-               learning_rate=0.001, batch_size=256, epochs=20):
+               learning_rate=0.001, batch_size=256, epochs=20, skip_params_unil=0):
     param_grid = dict(
         depth=[3, 4, 5],
         kernel_size=[3, 4, 5, 6],
         filters=[16, 32, 64, 96, 128, 256],
-        dropout_rate=np.arange(0.0, 0.81, 0.2),
+        dropout_rate=np.arange(0.0, 0.751, 0.25),
         normalize_inputs=[True, False]
     )
 
-    for hyperparams in ParameterGrid(param_grid):
+    for i, hyperparams in enumerate(ParameterGrid(param_grid)):
+        if i < skip_params_unil:
+            continue
+        print(i, hyperparams)
         robotics.run_training(data_path, log_path, output_path,
                               learning_rate=learning_rate, batch_size=batch_size, epochs=epochs,
                               **hyperparams)
